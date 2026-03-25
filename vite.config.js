032fd2ef16f9ff1b3e30a35107ad10f9953c59e8
Shijastk <shijasmuhammed573@gmail.com>
2026-03-25 20:53:51 +0530

    Add legacy plugin and polyfills for older TVs and Emulators

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import legacy from '@vitejs/plugin-legacy'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss()
+    tailwindcss(),
+    legacy({
+      targets: ['defaults', 'not IE 11', 'Chrome 60']
+    })
   ],
   base: './', // CRITICAL for Capacitor blank screen fix
   server: {
